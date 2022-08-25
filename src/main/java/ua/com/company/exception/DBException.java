e67ce92a038b1bab0760746be29c81d2184c877b
Bohdan Shevyrov <bshevyrov@gmail.com>
2022-08-26 02:42:02 +0300

    Create DBException and ADD pom mysql connector

---
+++ src/main/java/ua/com/company/exception/DBException.java
@@ -0,0 +1,11 @@
+package ua.com.company.exception;
+
+public class DBException extends Exception{
+    public DBException(String message) {
+        super(message);
+    }
+
+    public DBException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
