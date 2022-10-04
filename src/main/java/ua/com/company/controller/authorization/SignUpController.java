7c3db7e31ced13cba19be0e1ec73a242d887469b
Bohdan Shevyrov <bshevyrov@gmail.com>
2022-10-04 03:53:11 +0300

    signup and CategoriesController

--- src/main/java/ua/com/company/controller/authorization/SignUpController.java
+++ src/main/java/ua/com/company/controller/authorization/SignUpController.java
@@ -1,4 +1,4 @@
-package ua.com.company.servlet;
+package ua.com.company.controller.authorization;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -6,6 +6,8 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import ua.com.company.dao.PersonDAO;
+import ua.com.company.dao.mysql.impl.MysqlPersonDAOImpl;
 import ua.com.company.entity.Person;
 import ua.com.company.service.PersonService;
 import ua.com.company.service.impl.PersonServiceImpl;
@@ -13,7 +15,7 @@ import ua.com.company.service.impl.PersonServiceImpl;
 import java.io.IOException;
 
 @WebServlet(name = "SignUpServlet", value = "/signup")
-public class SignUpServlet extends HttpServlet {
+public class SignUpController extends HttpServlet {
 
 
     private void processRequest(HttpServletRequest request, HttpServletResponse response) {
@@ -39,15 +41,37 @@ public class SignUpServlet extends HttpServlet {
     protected void doPost(
             HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        Person person = new Person();
         String email = request.getParameter("email");
+        String username = request.getParameter("username");
         String password = request.getParameter("password");
-        person.setEmail(email);
-        person.setPassword(password);
-        PersonService personService = new PersonServiceImpl();
-        personService.create(person);
+        PersonDAO personDAO = new MysqlPersonDAOImpl();
+        PersonService personService = new PersonServiceImpl(personDAO);
+        if(
+        isValid(request, email, username, personService)){
+            Person person = new Person();
+            person.setEmail(email);
+            person.setUsername(username);
+            person.setPassword(password);
+            personService.create(person);
+
+        }
+
+
 
 
         processRequest(request, response);
+    }
+
+    private boolean isValid(HttpServletRequest request, String email, String username, PersonService personService) {
+        boolean rsl = true;
+        if (personService.findByEmail(email).isPresent()) {
+            request.setAttribute("email", "true");
+            rsl = false;
+        } else if (personService.findByUsername(username).isPresent()) {
+            request.setAttribute("username", "true");
+            rsl = false;
+        }
+
+        return rsl;
     }
 }
