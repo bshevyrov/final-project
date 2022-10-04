d08de8874c0f4064f8543f49033aae36103693a7
Bohdan Shevyrov <bshevyrov@gmail.com>
2022-10-04 03:54:20 +0300

    footer.jspf pom properties

--- src/main/java/ua/com/company/exception/DBException.java
+++ src/main/java/ua/com/company/exception/DBException.java
@@ -8,4 +8,8 @@ public class DBException extends Exception{
     public DBException(String message, Throwable cause) {
         super(message, cause);
     }
+
+    public DBException(Throwable cause) {
+        super(cause);
+    }
 }
