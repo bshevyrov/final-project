240b6db415e035866073b2d27677e6f059a7237e
Bohdan Shevyrov <bshevyrov@gmail.com>
2023-01-15 17:23:10 +0200

    person data validation

--- src/main/java/ua/com/company/view/controller/user/UserDetailsUpdateController.java
+++ src/main/java/ua/com/company/view/controller/user/UserDetailsUpdateController.java
@@ -1,5 +1,6 @@
 package ua.com.company.view.controller.user;
 
+import com.mysql.cj.util.StringUtils;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -15,12 +16,17 @@ import ua.com.company.view.dto.PersonAddressDTO;
 import ua.com.company.view.dto.PersonDTO;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UserDetailsUpdateController extends HttpServlet {
     private PersonFacade personFacade;
     private PersonAddressFacade personAddressFacade;
     private ImageFacade imageFacade;
     private PersonDTO personDTO;
+    private Set<String> errorFields;
 
     private final Logger log = LogManager.getLogger(UserDetailsUpdateController.class);
 
@@ -29,6 +35,8 @@ public class UserDetailsUpdateController extends HttpServlet {
         personFacade = (PersonFacade) getServletContext().getAttribute("personFacade");
         personAddressFacade = (PersonAddressFacade) getServletContext().getAttribute("personAddressFacade");
         imageFacade = (ImageFacade) getServletContext().getAttribute("imageFacade");
+        errorFields = new HashSet<>();
+
     }
 
     private void processRequest(HttpServletRequest request, HttpServletResponse response) {
@@ -45,67 +53,148 @@ public class UserDetailsUpdateController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int personId = ((PersonDTO) request.getSession().getAttribute("loggedPerson")).getId();
         personDTO = personFacade.findById(personId);
-        request.setAttribute("person", personFacade.findById(personId));
+        request.setAttribute("person", personDTO);
+        request.setAttribute("errorFields", errorFields);
+
         processRequest(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        PersonDTO currentPersonDTO = new PersonDTO();
-        currentPersonDTO.setId(personDTO.getId());
+        errorFields.clear();
+        PersonDTO newPersonDTO = new PersonDTO();
+        int id = personDTO.getId();
+        newPersonDTO.setId(id);
         String newUsername = request.getParameter("username");
         String newEmail = request.getParameter("email");
         String avatarPath = request.getParameter("avatarPath");
-        boolean updatable = false;
-        if (!avatarPath.equals(personDTO.getAvatar().getPath())) {
-            imageFacade.create(new ImageDTO("User " + currentPersonDTO.getId() + " avatar", avatarPath));
-            currentPersonDTO.setAvatar(imageFacade.findByPath(avatarPath));
-            updatable = true;
-        } else {
-            currentPersonDTO.setAvatar(personDTO.getAvatar());
-        }
-        if (!personDTO.getUsername().equals(newUsername)) {
-            updatable = true;
-            if (!personFacade.isExistByUsername(newUsername)) {
-                currentPersonDTO.setUsername(newUsername);
-            } else {
-                request.setAttribute("usernameExist", true);
-                request.setAttribute("person", personDTO);
-                processRequest(request, response);
-            }
-        } else {
-            currentPersonDTO.setUsername(personDTO.getUsername());
-        }
-        if (!personDTO.getEmail().equals(newEmail)) {
-            updatable = true;
-            if (!personFacade.isExistByEmail(newEmail)) {
-                currentPersonDTO.setEmail(newEmail);
-            } else {
+        ImageDTO imageDTO = new ImageDTO();
+        imageDTO.setName(id + " avatar");
+        imageDTO.setPath(avatarPath);
+        newPersonDTO.setAvatar(imageDTO);
+
+        newPersonDTO.setUsername(newUsername);
+        newPersonDTO.setEmail(newEmail);
+
+        if (!samePersonData(newPersonDTO)) {
+            if (personFacade.isExistByEmail(newEmail)) {
                 request.setAttribute("emailExist", true);
                 request.setAttribute("person", personDTO);
+                request.setAttribute("errorFields", errorFields);
                 processRequest(request, response);
+                return;
+            }
+            if (personFacade.isExistByUsername(newUsername)) {
+                request.setAttribute("usernameExist", true);
+                request.setAttribute("person", personDTO);
+                request.setAttribute("errorFields", errorFields);
+                processRequest(request, response);
+                return;
+            }
+            processRequest(request, response);
+            if (!validUserData(newPersonDTO)) {
+                request.setAttribute("person", newPersonDTO);
+                request.setAttribute("errorFields", errorFields);
+                processRequest(request, response);
+                return;
+            }
+            personFacade.update(newPersonDTO);
+        }
+
+        PersonAddressDTO newPersonAddressDTO = getPersonAddressDTO(request);
+        newPersonDTO.setPersonAddressDTO(newPersonAddressDTO);
+        errorFields.clear();
+        if (!samePersonAddressData(newPersonAddressDTO)) {
+            if (!validUserAddressData(newPersonAddressDTO)) {
+                request.setAttribute("person", newPersonDTO);
+                request.setAttribute("errorFields", errorFields);
+                processRequest(request, response);
+                return;
+            }
+            if (personDTO.getPersonAddressDTO().getId() != 0) {
+                personAddressFacade.update(newPersonAddressDTO);
+            } else {
+                personAddressFacade.create(newPersonAddressDTO);
+            }
+        }
+
+/*        if (personDTO.getPersonAddressDTO() != null) {
+            if (!personDTO.getPersonAddressDTO().equals(newPersonAddressDTO)) {
+                newPersonAddressDTO.setId(personDTO.getPersonAddressDTO().getId());
+                personAddressFacade.update(newPersonAddressDTO);
             }
         } else {
-            currentPersonDTO.setEmail(personDTO.getEmail());
-        }
-        if (updatable) {
-            personFacade.update(currentPersonDTO);
-        }
-
-
-        PersonAddressDTO personAddressDTO = getPersonAddressDTO(request);
-
-        if (personDTO.getPersonAddressDTO() != null) {
-            if (!personDTO.getPersonAddressDTO().equals(personAddressDTO)) {
-                personAddressDTO.setId(personDTO.getPersonAddressDTO().getId());
-                personAddressFacade.update(personAddressDTO);
-            }
-        } else {
-            personAddressFacade.create(personAddressDTO);
-        }
+            personAddressFacade.create(newPersonAddressDTO);
+        }*/
 
         response.sendRedirect("/user/details");
+}
+
+    private boolean validUserAddressData(PersonAddressDTO newPersonAddressDTO) {
+       
+        stringValidation("city", newPersonAddressDTO.getCity());
+        stringValidation("country", newPersonAddressDTO.getCountry());
+        phoneValidation("phone", newPersonAddressDTO.getPhone());
+        stringValidation("firstName", newPersonAddressDTO.getFirstName());
+        stringValidation("lastName", newPersonAddressDTO.getLastName());
+        numberValidation("postalCode", newPersonAddressDTO.getPostalCode());
+        stringValidation("address", newPersonAddressDTO.getAddress());
+        return errorFields.isEmpty();
+
+    }
+
+    private void phoneValidation(String fieldName, String value) {
+        if (StringUtils.isNullOrEmpty(value)
+                || StringUtils.isEmptyOrWhitespaceOnly(value)
+                || !StringUtils.isStrictlyNumeric(value)) {
+            errorFields.add(fieldName);
+        }
+    }
+
+    private boolean validUserData(PersonDTO newPersonDTO) {
+        stringValidation("username", newPersonDTO.getUsername());
+        emailValidation("email", newPersonDTO.getEmail());
+        return errorFields.isEmpty();
+    }
+
+    private void numberValidation(String fieldName, double value) {
+        if (value <= 0) {
+            errorFields.add(fieldName);
+        }
+    }
+
+    private void emailValidation(String fieldName, String emailAddress) {
+        Pattern regexPattern = Pattern.compile("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+                + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$");
+        Matcher m = regexPattern.matcher(emailAddress);
+        if (m.matches()) {
+            errorFields.add(fieldName);
+        }
+    }
+
+    private void stringValidation(String fieldName, String value) {
+        if (StringUtils.isNullOrEmpty(value)
+                || StringUtils.isEmptyOrWhitespaceOnly(value)
+                || StringUtils.isStrictlyNumeric(value)) {
+            errorFields.add(fieldName);
+        }
+    }
+
+    private boolean samePersonAddressData(PersonAddressDTO newPersonAddressDTO) {
+        PersonAddressDTO currentPersonAddressDTO = personDTO.getPersonAddressDTO();
+        return newPersonAddressDTO.getAddress().equals(currentPersonAddressDTO.getAddress())
+                && newPersonAddressDTO.getCity().equals(currentPersonAddressDTO.getCity())
+                && newPersonAddressDTO.getCountry().equals(currentPersonAddressDTO.getCountry())
+                && newPersonAddressDTO.getFirstName().equals(currentPersonAddressDTO.getFirstName())
+                && newPersonAddressDTO.getLastName().equals(currentPersonAddressDTO.getLastName())
+                && newPersonAddressDTO.getPhone().equals(currentPersonAddressDTO.getPhone())
+                && newPersonAddressDTO.getPostalCode() == (currentPersonAddressDTO.getPostalCode());
+    }
+
+    private boolean samePersonData(PersonDTO newPersonDTO) {
+        return newPersonDTO.getAvatar().getPath().equals(personDTO.getAvatar().getPath())
+                && newPersonDTO.getEmail().equals(personDTO.getEmail())
+                && newPersonDTO.getUsername().equals(personDTO.getUsername());
     }
 
     private PersonAddressDTO getPersonAddressDTO(HttpServletRequest request) {
--- src/main/webapp/WEB-INF/jsp/user/user-details-update.jsp
+++ src/main/webapp/WEB-INF/jsp/user/user-details-update.jsp
@@ -23,7 +23,7 @@
                     <h5><fmt:message key="user.span.avatar"/></h5>
                 </div>
                 <div class="img-container">
-                    <img src="${person.avatar.path}" id="avatar" />
+                    <img src="${person.avatar.path}" id="avatar"/>
                 </div>
 
                 <div class="input__item">
@@ -31,34 +31,12 @@
                     </label><input type="text" name="avatarPath" id="coverPath" value="${person.avatar.path}" hidden>
                 </div>
                 <div>
-
-                            <input
-                                    class="form-control form-control-lg"
-                                    id="selectAvatar"
-                                    type="file" accept="image/png, image/jpeg"
-                            />
-
+                    <input
+                            class="form-control form-control-lg"
+                            id="selectAvatar"
+                            type="file" accept="image/png, image/jpeg"
+                    />
                 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
                 <div class="section-title">
                     <h5><fmt:message key="user.span.email"/></h5>
@@ -68,6 +46,11 @@
                     <c:if test="${true.equals(emailExist)}">
                         <small><fmt:message key="user.details.email.exist"/></small>
                     </c:if>
+                    <div class="error_small">
+                        <c:if test="${con:contains(errorFields,\"email\")}">
+                            <small> <fmt:message key="user.details.error.email"/></small>
+                        </c:if>
+                    </div>
                 </div>
                 <div class="section-username">
                     <h5><fmt:message key="user.span.username"/></h5>
@@ -76,6 +59,11 @@
                     <c:if test="${true.equals(usernameExist)}">
                         <small><fmt:message key="user.details.username.exist"/></small>
                     </c:if>
+                    <div class="error_small">
+                        <c:if test="${con:contains(errorFields,\"username\")}">
+                            <small> <fmt:message key="user.details.error.username"/></small>
+                        </c:if>
+                    </div>
                 </div>
                 <div class="section-price">
                     <h5><fmt:message key="user.span.deposit.funds"/></h5>
@@ -90,42 +78,77 @@
                     <br/>
                     <input placeholder="<fmt:message key="user.details.name.first"/>" name="firstName"
                            value="${person.personAddressDTO.firstName}" required>
+                    <div class="error_small">
+                        <c:if test="${con:contains(errorFields,\"firstName\")}">
+                            <small> <fmt:message key="user.details.error.firstName"/></small>
+                        </c:if>
+                    </div>
                 </div>
                 <div class="section-title">
                     <h5><fmt:message key="user.details.name.last"/></h5>
                     <br/>
                     <input placeholder="<fmt:message key="user.details.name.last"/>" name="lastName"
                            value="${person.personAddressDTO.lastName}" required>
+                    <div class="error_small">
+                        <c:if test="${con:contains(errorFields,\"lastName\")}">
+                            <small> <fmt:message key="user.details.error.lastName"/></small>
+                        </c:if>
+                    </div>
                 </div>
                 <div class="section-title">
                     <h5><fmt:message key="user.details.address"/></h5>
                     <br/>
                     <input placeholder="<fmt:message key="user.details.address"/>" name="address"
                            value="${person.personAddressDTO.address}" required>
+                    <div class="error_small">
+                        <c:if test="${con:contains(errorFields,\"address\")}">
+                            <small> <fmt:message key="user.details.error.address"/></small>
+                        </c:if>
+                    </div>
                 </div>
                 <div class="section-title">
                     <h5><fmt:message key="user.details.city"/></h5>
                     <br/>
                     <input placeholder="<fmt:message key="user.details.city"/>" name="city"
                            value="${person.personAddressDTO.city}" required>
+                    <div class="error_small">
+                        <c:if test="${con:contains(errorFields,\"city\")}">
+                            <small> <fmt:message key="user.details.error.city"/></small>
+                        </c:if>
+                    </div>
                 </div>
                 <div class="section-title">
                     <h5><fmt:message key="user.details.country"/></h5>
                     <br/>
                     <input placeholder="<fmt:message key="user.details.country"/>" name="country"
                            value="${person.personAddressDTO.country}" required>
+                    <div class="error_small">
+                        <c:if test="${con:contains(errorFields,\"country\")}">
+                            <small> <fmt:message key="user.details.error.country"/></small>
+                        </c:if>
+                    </div>
                 </div>
                 <div class="section-title">
                     <h5><fmt:message key="user.details.phone"/></h5>
                     <br/>
                     <input placeholder="<fmt:message key="user.details.phone"/>" name="phone" type="number"
                            value="${person.personAddressDTO.phone}" required>
+                    <div class="error_small">
+                        <c:if test="${con:contains(errorFields,\"phone\")}">
+                            <small> <fmt:message key="user.details.error.phone"/></small>
+                        </c:if>
+                    </div>
                 </div>
                 <div class="section-title">
                     <h5><fmt:message key="user.details.postal.code"/></h5>
                     <br/>
                     <input placeholder="<fmt:message key="user.details.postal.code"/>" name="postalCode" type="number"
                            value="${person.personAddressDTO.postalCode}" required>
+                    <div class="error_small">
+                        <c:if test="${con:contains(errorFields,\"postalCode\")}">
+                            <small> <fmt:message key="user.details.error.postalCode"/></small>
+                        </c:if>
+                    </div>
                 </div>
                 <div>
                     <button type="submit" <%--onclick="return validateForm();"--%> class="site-btn" id="check-btn">
